3a52e2bc8d4a43a923735431047426c8eb04fef3
Anubhav Singh <68738228+anubhavsingh0708@users.noreply.github.com>
2022-05-06 11:32:07 +0530

    update

---
+++ play.js
@@ -0,0 +1,51 @@
+var c = document.getElementById("gCanvas");
+var ctx = c.getContext("2d");
+var a=[0,0];
+var a=[0,0];
+var a=[0,0];
+var a=[0,0];
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(a[0], a[1], 100, 100);
+ctx.font = "30px Comic Sans MS";
+ctx.strokeText("1", a[0]+40, a[1]+40); 
+
