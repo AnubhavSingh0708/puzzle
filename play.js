62c95fa9dd134d00a2b3bac6837796dd2f4e5421
Anubhav Singh <68738228+anubhavsingh0708@users.noreply.github.com>
2022-05-07 10:57:57 +0530

    mde numbers

--- play.js
+++ play.js
@@ -1,51 +1,43 @@
+var nums = [1,2,3,4,5,6,7,8],//all numbers to be randomized
+    ranNums = [],
+    i = nums.length,
+    j = 0;
+
+while (i--) {
+    j = Math.floor(Math.random() * (i+1));
+    ranNums.push(nums[j]);
+    nums.splice(j,1);
+}
 var c = document.getElementById("gCanvas");
 var ctx = c.getContext("2d");
-var a=[0,0];
-var a=[0,0];
-var a=[0,0];
-var a=[0,0];
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+var w = c.width;
+var h = c.height;
+var a1=[1,1];
+var a2=[w/3,1];
+var a3=[w*2/3,1];
+var b1=[1,h/3];
+var b2=[w/3,h/3];
+var b3=[w*2/3,h/3];
+var c1=[1,h*2/3];
+var c2=[w/3,h*2/3];
+var c3=[w*2/3,h*2/3];
+ctx.font = "30px Arial";
+ctx.strokeText(ranNums[0], a1[0]+40, a1[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+ctx.strokeText(ranNums[1], a2[0]+40, a2[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+ctx.strokeText(ranNums[2], a3[0]+40, a3[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+ctx.strokeText(ranNums[3], b1[0]+40, b1[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+ctx.strokeText(ranNums[4], b2[0]+40, b2[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+ctx.strokeText(ranNums[5], b3[0]+40, b2[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+ctx.strokeText(ranNums[6], c1[0]+40, c1[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+ctx.strokeText(ranNums[7], c2[0]+40, c2[1]+40); 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(a[0], a[1], 100, 100);
-ctx.font = "30px Comic Sans MS";
-ctx.strokeText("1", a[0]+40, a[1]+40); 
+c.addEventListener("click",function() {
 
+},false)
