68e6d952c7ba68eaf771c862606ab89c7bac699c
Anubhav Singh <68738228+anubhavsingh0708@users.noreply.github.com>
2022-05-11 09:41:59 +0530

    added movement detection

--- play.js
+++ play.js
@@ -21,7 +21,7 @@ var b3=[w*2/3,h/3,true];
 var c1=[1,h*2/3,true];
 var c2=[w/3,h*2/3,true];
 var c3=[w*2/3,h*2/3,false];
-var movement=[""+""];//[origin,destination]
+var movement=[""+""+""+""];//[origin,destination]
 var cRect;
 var canvasX;
 var canvasY;
@@ -47,13 +47,30 @@ c.addEventListener('mousedown', function(e) {
     cRect = c.getBoundingClientRect();       
      canvasX = Math.round(e.clientX - cRect.left);  
      canvasY = Math.round(e.clientY - cRect.top);  
-  if (canvasX<1){
-      
-  }
+   movement[0]="" ;
+   movement[1]="";
+  if (canvasY<h/3){movement[0]+="a"}
+  else if(canvasY<(2*h/3)){movement[0]+="b"}
+  else if(canvasY>(2*h/3)){movement[0]+="c"}
+  if (canvasX<w/3){movement[1]+="1"}
+  else if(canvasX<(2*w/3)){movement[1]+="2"}
+  else if(canvasX>(2*w/3)){movement[1]+="3"}
 });
 c.addEventListener('mouseup', function(f) { 
      cRect = c.getBoundingClientRect();        
      canvasX = Math.round(f.clientX - cRect.left);  
-     canvasY = Math.round(f.clientY - cRect.top);   
- 
+     canvasY = Math.round(f.clientY - cRect.top);  
+     movement[2]="" ;
+     movement[3]=""; 
+     if (canvasY<h/3){movement[2]+="a"}
+     else if(canvasY<(2*h/3)){movement[2]+="b"}
+     else if(canvasY>(2*h/3)){movement[2]+="c"}
+     if (canvasX<w/3){movement[3]+="1"}
+     else if(canvasX<(2*w/3)){movement[3]+="2"}
+     else if(canvasX>(2*w/3)){movement[3]+="3"}
+    console.log(movement[2]+movement[3]);
+    checkMov();//checks if movement is possible
 });
+function checkMov(){
+    
+}
